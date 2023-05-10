7a4878fd55693384182ed1793ff17c9c890d004b
圈圈 <vvvvvvii@yahoo.com.tw>
2023-05-10 10:46:42 +0800

    Step One / ImageShowBtn
    圖片改抓 image.urls.small
    圖片結合 LazyLoadImage 效果

--- src/components/ImageShowBtn.js
+++ src/components/ImageShowBtn.js
@@ -1,3 +1,5 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 function ImageShowBtn({ image, onChangeBackgroundUrl, selectedImageUrl }) {
   const handleClick = (url) => {
     onChangeBackgroundUrl(url);
@@ -9,12 +11,13 @@ function ImageShowBtn({ image, onChangeBackgroundUrl, selectedImageUrl }) {
       className="col-3 mb-6 search-result-btn"
       onClick={() => handleClick(image.urls.full)}
     >
-      <img
-        src={image.urls.full}
+      <LazyLoadImage
         alt={image.alt_description}
+        src={image.urls.small}
         className={`search-result-img ${
           selectedImageUrl === image.urls.full ? "selected-img" : ""
         }`}
+        effect="blur"
       />
     </button>
   );
